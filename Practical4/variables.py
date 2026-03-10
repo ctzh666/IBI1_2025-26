833a7027e10c8ac2b4478acdc3f6de8a2ec8e3eb
Chen Taozehan <taozehan.25@intl.zju.edu.cn>
2026-03-10 17:07:04 +0800

    finish

---
+++ Practical4/variables.py
@@ -0,0 +1,16 @@
+a = 508
+b = 533
+c = 555
+d = b-a
+e = c-b
+print (d, "and", e)#d大于e，人口增加减速
+X = True 
+Y = False
+W = X or Y
+print(W)
+# W 的真值表 (Truth Table for W = X or Y):
+# |  X   |  Y   |  W = X or Y  |
+# | True | True |    True      |
+# | True | False|    True      |
+# | False| True |    True      |
+# | False| False|    False     |
