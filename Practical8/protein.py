c92993e895ed225d4f2aafe543683e00d6ce8c18
ctzh666 <taozehan.25@intl.zju.edu.cn>
2026-05-10 18:02:53 +0800

    Add files via upload

---
+++ Practical8/protein.py
@@ -0,0 +1,9 @@
+aminoacid = {'G': 57.02,'A': 71.04,'S': 87.03,'P': 97.05,'V': 99.07,'T': 101.05,'C': 103.01,'I': 113.08,'L': 113.08,'N': 114.04,'D': 115.03,'Q': 128.06,'K': 128.09,'E': 129.04,'M': 131.04,'H': 137.06,'F': 147.07,'R': 156.10,'Y': 163.06}
+def calculate(sequence):
+    total_mass = 0.0 
+    for aa in sequence: 
+        if aa not in aminoacid:
+            raise ValueError(f"Error")
+        total_mass += aminoacid[aa]
+    return total_mass
+print(f"ACG: {calculate('ACG'):.2f}")
